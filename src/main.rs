d33cb8a5b3002067271f1a896170861f2d124e73
Hugo Laloge <hugo.laloge@gmail.com>
2018-04-06 11:49:50 +0100

    feat: add color output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+extern crate termcolor;
 extern crate validate_commit;
 
+use std::io::Write;
 use std::process::exit;
+
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn main() {
     if std::env::args().len() != 2 {
@@ -12,7 +16,17 @@ fn main() {
         .nth(1)
         .unwrap();
     if let Err(e) = validate_commit::validate_commit_file(&file_path) {
-        eprintln!("error: {}", e);
+        write_error(e);
         exit(1);
     }
+}
+
+fn write_error(error: validate_commit::Error) {
+    let formatted_error = format!("{}", error);
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+    stdout.set_color(ColorSpec::new().set_bold(true).set_fg(Some(Color::Red)))
+        .and_then(|()| stdout.write_all(b"error: "))
+        .and_then(|()| stdout.reset())
+        .and_then(|()| stdout.write_fmt(format_args!("{}\n", formatted_error)))
+        .expect(&formatted_error);
 }
