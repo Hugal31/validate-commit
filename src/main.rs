cf05075ca74cd186a8427c80595bc9ef4fb4f19e
Hugo Laloge <hugo.laloge@gmail.com>
2018-04-06 13:44:38 +0100

    refactor: use reference where needed

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,12 @@ fn main() {
 
     let file_path = std::env::args().nth(1).unwrap();
     if let Err(e) = validate_commit::validate_commit_file(&file_path) {
-        write_error(e);
+        write_error(&e);
         exit(1);
     }
 }
 
-fn write_error(error: validate_commit::Error) {
+fn write_error(error: &validate_commit::Error) {
     let formatted_error = format!("{}", error);
     let mut stdout = StandardStream::stdout(ColorChoice::Auto);
     stdout
